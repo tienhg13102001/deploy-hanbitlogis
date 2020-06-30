75c68f8aec833ed6e400eac7d1b5fed146a0d880
jongho143 <jongho.lee@playlooc.com>
2020-06-30 12:09:31 +0900

    회사소개 부분 퍼블리싱 완료

--- src/Pages/Introduction/function/Come.js
+++ src/Pages/Introduction/function/Come.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './style/Come.scss'
 
+
 class Come extends Component {
     render() {
         return (
@@ -27,7 +28,7 @@ class Come extends Component {
                     </div>
                 </div>
                 <div className="Hanbit_Map">
-                    
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.625365785722!2d127.04003211531028!3d37.5167531798072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca4751976494d%3A0x121b7bb610d91f44!2z7ISc7Jq47Yq567OE7IucIOqwleuCqOq1rCDsgrzshLHrj5kg7ZWZ64-Z66GcNTbquLggOA!5e0!3m2!1sko!2skr!4v1593485547469!5m2!1sko!2skr" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe>
                 </div>
             </div>
         );
