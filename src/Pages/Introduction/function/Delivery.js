16a969e42f9cbc0b0eff9e72ddb2182543de292b
hippocoli <junghoon0112@gmail.com>
2020-07-20 23:09:39 +0900

    한빛 로지스 > 디테일 수정

--- src/Pages/Introduction/function/Delivery.js
+++ src/Pages/Introduction/function/Delivery.js
@@ -1,26 +1,112 @@
 import React, { Component } from 'react';
 import './style/Delivery.scss'
-
 import icon01 from '../../../_asset/image/Introduction/point-icon-1.png';
 import icon02 from '../../../_asset/image/Introduction/point-icon-2.png';
 import icon03 from '../../../_asset/image/Introduction/point-icon-3.png';
-
 import icon from '../../../_asset/image/Introduction/point-icon-4.png';
+import { Steps, Divider, Space, Card, Table, Tag } from 'antd';
+
+const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: tags => (
+        <>
+          {tags.map(tag => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+  
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
+const { Step } = Steps;
 
 class Delivery extends Component {
+
+
+
+
+    state = {
+        current: 0,
+    };
+
+    onChange = current => {
+        console.log('onChange:', current);
+        this.setState({ current });
+    };
     list = [
-        { id: 0, text: '송도착시간 예고제 실시' },
-        { id: 1, text: '각 배송원의 인성 및 service-mind 정기교육 (3개월 단위)' },
-        { id: 2, text: '배송 복장의 통일 (유니폼 및 모자 착용)' },
-        { id: 3, text: '차량의 청결 상태 점검 (월 1회)' },
-        { id: 4, text: '배송 일정 점검에 의한 기사 평가제 실시' },
-        { id: 5, text: '배송 도착시간 예고제 실시' },
-        { id: 6, text: '전 차량 Real-Time 상품 추적과 System 장비 구비 및 관찰' },
-        { id: 7, text: '투입 인원에 대한 밀착형 인원관리 (mind-upgrade)' },
-        { id: 8, text: '배송 차량 사고에 대비한 비상 연락체계 확립' },
+        { id: 0, principleNumber: '제 1 원칙', text: '배송도착시간 예고제 실시' },
+        { id: 1, principleNumber: '제 2 원칙', text: '각 배송원의 인성 및 service-mind 정기교육 (3개월 단위)' },
+        { id: 2, principleNumber: '제 3 원칙', text: '배송 복장의 통일 (유니폼 및 모자 착용)' },
+        { id: 3, principleNumber: '제 4 원칙', text: '차량의 청결 상태 점검 (월 1회)' },
+        { id: 4, principleNumber: '제 5 원칙', text: '배송 일정 점검에 의한 기사 평가제 실시' },
+        { id: 5, principleNumber: '제 6 원칙', text: '배송 도착시간 예고제 실시' },
+        { id: 6, principleNumber: '제 7 원칙', text: '전 차량 Real-Time 상품 추적과 System 장비 구비 및 관찰' },
+        { id: 7, principleNumber: '제 8 원칙', text: '투입 인원에 대한 밀착형 인원관리 (mind-upgrade)' },
+        { id: 8, principleNumber: '제 9 원칙', text: '배송 차량 사고에 대비한 비상 연락체계 확립' },
     ]
     checklist = [
-        { icon: icon01, title: '1 Point', text: '송도착시간 예고제를 실시합니다.' },
+        { icon: icon01, title: '1 Point', text: '배송도착시간 예고제를 실시합니다.' },
         { icon: icon02, title: '2 Point', text: '차량의 청결 상태를 월 1회 점검합니다.' },
         { icon: icon03, title: '3 Point', text: '배송 도착시간 예고제를 실시합니다.' },
     ]
@@ -33,8 +119,11 @@ class Delivery extends Component {
         { text: '차량의 사양에 대한 만족도 확인' }
     ]
     render() {
+        const { current } = this.state;
+
         return (
             <div className="DeliContainer">
+
                 <div className="Wrap_Line">
                     <div className="Title">
                         <span>경영</span> 방침
@@ -44,15 +133,32 @@ class Delivery extends Component {
                         한걸음 한걸음 고객의 행복을 채우는 발자취를<br />
                         새롭게 만들어 가겠습니다.
                     </div>
+                    <Divider />
                     <div className="DeliTitle">
                         <div className="Text">
                             Delivery luck
                         </div>
                         <div className="Operation">
-                            배송운영
+                            운영 방침
                         </div>
                     </div>
-                    <div className="Table">
+                    {/* <Steps current={current} onChange={this.onChange}>
+                        <Step title="원칙 1" description="배송 도착시간 예고제 실시" />
+                        <Step title="원칙 2" description="각 배송원의 인성 및 service-mind 정기교육 (3개월 단위)" />
+                        <Step title="원칙 3" description="배송 복장의 통일 (유니폼 및 모자 착용)" />
+                    </Steps> */}
+
+                    <Steps current={9} onChange={this.onChange} direction="vertical">
+
+                        {this.list.map((item, index) => {
+                            return (
+                                <Step key={index} title={item.principleNumber} description={item.text} />
+                            )
+                        })}
+                    </Steps>
+                    <Divider />
+
+                    {/* <div className="Table">
                         <div className="Table_Box">
                             {this.list.map((item, index) => {
                                 return (
@@ -60,11 +166,12 @@ class Delivery extends Component {
                                 )
                             })}
                         </div>
-                    </div>
+                    </div> */}
+
                     <div className="CheckPoint">
                         <div className="Check_TitleBox">
                             <div className="Check_Text">
-                                Check Point
+                                Check Points
                             </div>
                         </div>
                         <div className="Check_List">
@@ -72,15 +179,15 @@ class Delivery extends Component {
                                 return (
                                     <div className="Check_Box" key={index}>
                                         <img className="Check_Icon" src={item.icon} alt='아이콘'></img>
-                                        <div className="Check_Title">{item.title}</div>
                                         <div className="Check_Text">{item.text}</div>
                                     </div>
                                 )
                             })}
                         </div>
+                        <Divider />
                         <div className="DeliTitle">
                             <div className="Text">
-                                follow-up management
+                                Follow-up management
                         </div>
                             <div className="Operation">
                                 사후 관리
@@ -96,7 +203,7 @@ class Delivery extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className="CheckPoint">
+                    {/* <div className="CheckPoint">
                         <div className="Check_TitleBox">
                             <div className="Check_Point">
                                 CheckPoint
@@ -104,10 +211,9 @@ class Delivery extends Component {
                             <div className="Check_Icon">
                                 <img src={icon} alt='아이콘'></img>
                             </div>
-                            <div className="Check_Text">Point</div>
                             <div className="Check_Title">전 배송처의 지리 (위치) 를 확보합니다.</div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         );
--- src/Pages/Introduction/function/HistoryContents.js
+++ src/Pages/Introduction/function/HistoryContents.js
@@ -1,6 +1,162 @@
 import React, { Component } from 'react';
 import './style/HistoryContents.scss'
+import { Table, Tag, Space } from 'antd';
 
+const columns = [
+  {
+    title: 'Year',
+    dataIndex: 'year',
+    key: 'year',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Month',
+    dataIndex: 'month',
+    key: 'month',
+  },
+  {
+    title: 'Details',
+    dataIndex: 'details',
+    key: 'details',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'black' : 'black';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+},
+];
+
+
+
+const data = [
+  {
+    key: '1',
+    year: '2015년',
+    month: '05월',
+    details: '홈페이지 개편',
+    tags: ['홈페이지', '리뉴얼'],
+  },
+  {
+    key: '2',
+    year: '2014년',
+    month: '01월',
+    details: '정보 시스템 부서 개설',
+    tags: ['시스템'],
+  },
+  {
+    key: '3',
+    year: '2013년',
+    month: '01월',
+    details: '서울특별시 지체 장애인 협회 협력사 등록',
+    tags: ['협력사'],
+  },
+  {
+    key: '3',
+    year: '2013년',
+    month: '01월',
+    details: '서울체신청 우수업체로 선정',
+    tags: ['우수업체 선정'],
+  },
+  {
+    key: '5',
+    year: '2013년',
+    month: '01월',
+    details: '서울시장 우수기업 표창',
+    tags: ['서울시장 표창'],
+  },
+  {
+    key: '6',
+    year: '2013년',
+    month: '01월',
+    details: '복합운송주선협등록 등록번호 (제 2315호) 전국 화물자동차 연합회 우수기업 표창',
+    tags: ['우수기업 표창'],
+  },
+  {
+    key: '7',
+    year: '2013년',
+    month: '01월',
+    details: '국방부 조당청 외자등록 사단법인 한국수입업협회 회원등록',
+    tags: ['협력사'],
+  },
+  {
+    key: '8',
+    year: '2013년',
+    month: '01월',
+    details: '경기도 연천군 합수리 차고지 200평 구입',
+    tags: ['협력사'],
+  },
+  {
+    key: '9',
+    year: '2013년',
+    month: '01월',
+    details: '한빛로지스 주식회사 대표이사 이정화 취임',
+    tags: ['협력사'],
+  },
+  {
+    key: '10',
+    year: '2013년',
+    month: '01월',
+    details: '한빛로지스 주식회사 본사(현) 사무실 이전',
+    tags: ['협력사'],
+  },
+  {
+    key: '11',
+    year: '2013년',
+    month: '01월',
+    details: '주식회사 한빛통운 설립',
+    tags: ['협력사'],
+  },
+  {
+    key: '12',
+    year: '2013년',
+    month: '01월',
+    details: '경기도 여주군 수정리 사무실 및 사택 준공',
+    tags: ['협력사'],
+  },
+  {
+    key: '13',
+    year: '2013년',
+    month: '01월',
+    details: '협력사별 50 - 100평 창고 2개 보유',
+    tags: ['협력사'],
+  },
+  {
+    key: '14',
+    year: '2013년',
+    month: '01월',
+    details: '21세기 종합물류 주식회사 창립',
+    tags: ['협력사'],
+  },
+  {
+    key: '15',
+    year: '2013년',
+    month: '01월',
+    details: '21세기 종합물류 주식회사 창립',
+    tags: ['협력사'],
+  },
+  {
+    key: '16',
+    year: '2013년',
+    month: '01월',
+    details: '본사 직영 200여평 2개소의 창고 준공',
+    tags: ['협력사'],
+  },
+];
 class HistoryContents extends Component {
     list = [
         { year: '2015년-', month: '05월', text: '홈페이지 개편' },
@@ -33,6 +189,7 @@ class HistoryContents extends Component {
                     </div>
                 </div>
                 <div className="His_List">
+                <Table  columns={columns} dataSource={data} />
                     {this.list.map((item, index) => {
                         return (
                             <div className="List_Box" key={index}>
