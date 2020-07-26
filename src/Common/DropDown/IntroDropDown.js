6b72a9cc35dee36bb89ab2bd7ea6ee1b804afc70
jongho143 <sumaoo20@naver.com>
2020-07-26 17:18:07 +0900

    메뉴탭 수정 및 드랍박스

--- src/Common/DropDown/IntroDropDown.js
+++ src/Common/DropDown/IntroDropDown.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import { withStyles, styled } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import React from "react";
+import { withStyles, styled } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import { NavLink } from "react-router-dom";
-import './DropDown.scss'
+import "./DropDown.scss";
 
 const StyledMenu = withStyles({
   paper: {
-    width:'100%',
-    border: '1px solid #d3d4d5',
-    maxWidth: "100%"
+    width: "100%",
+    border: "1px solid #d3d4d5",
+    maxWidth: "100%",
   },
-
 })((props) => (
   <Menu
     elevation={0}
     getContentAnchorEl={null}
     anchorOrigin={{
-      vertical: 'bottom',
+      vertical: "bottom",
       horizontal: "center",
     }}
     transformOrigin={{
-      vertical: 'top',
+      vertical: "top",
       horizontal: "center",
     }}
     {...props}
@@ -31,40 +30,30 @@ const StyledMenu = withStyles({
 ));
 
 const MyButton = styled(Button)({
-  width:'100%',
-  fontSize:'16px',
-  background: '#da2320',
+  width: "100%",
+  fontSize: "16px",
+  background: "#da2320",
   border: 0,
   borderRadius: 3,
-  color: 'white',
+  color: "white",
   height: 48,
-  padding: '0 30px',
-  '&:hover': {
+  padding: "0 30px",
+  "&:hover": {
     backgroundColor: "#da2320",
   },
 });
 
-
 const StyledMenuItem = withStyles((theme) => ({
   root: {
-    width:"100%",
-    color:'#333333',
-    justifyContent:'center',
-    '&:focus': {
-      width:'100%',
-      backgroundColor: "#da2320",
-      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-        color: '#fff'
-      },
-    },
+    width: "100%",
+    color: "#333333",
+    justifyContent: "center",
   },
 }))(MenuItem);
 
-
-
 export default function CustomizedMenus() {
   const [anchorEl, setAnchorEl] = React.useState(null);
-
+  const [menuText, setMenuText] = React.useState("회사 소개");
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -73,18 +62,21 @@ export default function CustomizedMenus() {
     setAnchorEl(null);
   };
 
+  const menuOnClick = (text) => {
+    setMenuText(text);
+    setAnchorEl(null);
+  };
+
   return (
     <div className="DropDown_container">
-      <MyButton 
+      <MyButton
         aria-controls="customized-menu"
         aria-haspopup="true"
         variant="contained"
         color="primary"
         onClick={handleClick}
       >
-        
-        회사 소개
-
+        {menuText}
       </MyButton>
       <StyledMenu
         id="customized-menu"
@@ -93,24 +85,22 @@ export default function CustomizedMenus() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <StyledMenuItem>
+        <StyledMenuItem onClick={() => menuOnClick("경영 방침")}>
           <NavLink className="Mobile_link" to="/Intro/Policy">
-          <ListItemText primary="경영 방침" />
+            <ListItemText primary="경영 방침" />
           </NavLink>
         </StyledMenuItem>
-        <StyledMenuItem>
+        <StyledMenuItem onClick={() => menuOnClick("회사 연혁")}>
           <NavLink className="Mobile_link" to="/Intro/history">
-          <ListItemText primary="회사 연혁" />
+            <ListItemText primary="회사 연혁" />
           </NavLink>
         </StyledMenuItem>
-        <StyledMenuItem>
+        <StyledMenuItem onClick={() => menuOnClick("오시는길")}>
           <NavLink className="Mobile_link" to="/Intro/come">
-          <ListItemText primary="오시는길" />
+            <ListItemText primary="오시는길" />
           </NavLink>
         </StyledMenuItem>
       </StyledMenu>
     </div>
   );
 }
-
-
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,114 +1,129 @@
-import React, { Component } from 'react';
-import { slide as BurMenu } from 'react-burger-menu'
+import React, { Component } from "react";
+import { slide as BurMenu } from "react-burger-menu";
 import { NavLink } from "react-router-dom";
-import './style/Navigation.scss'
+import "./style/Navigation.scss";
 
-import closeicon from '../../_asset/image/common/xbotton.png'
-import logo from '../../_asset/image/common/logo.png'
-import menuicon from '../../_asset/image/common/menu-icon.png'
-import Guide from '../Guide/Guide';
-
-
-
+import closeicon from "../../_asset/image/common/xbotton.png";
+import logo from "../../_asset/image/common/logo.png";
+import menuicon from "../../_asset/image/common/menu-icon.png";
+import Guide from "../Guide/Guide";
 
 class Navigation extends Component {
-    state = {
-        isClickedMenu: false,
-        clickedMenu: ""
+  state = {
+    isClickedMenu: false,
+    clickedMenu: "",
+  };
+  movePage = (pageName) => {
+    this.props.history.push({
+      pathname: pageName,
+    });
+  };
+  handleClickMenu = (clickedMenu) => {
+    const { clickedMenu: clickedMeneName, isClickedMenu } = this.state;
+    if (clickedMeneName === clickedMenu && isClickedMenu) {
+      this.setState({ isClickedMenu: false });
+    } else {
+      this.setState({ clickedMenu, isClickedMenu: true });
     }
-    movePage = (pageName) => {
-        this.props.history.push({
-            pathname: pageName
-        })
+  };
+  leaveMenu = () => {
+    this.setState({ isClickedMenu: false });
+  };
+  renderSubMenu = (clickedMenu) => {
+    let subMenu;
+    const { isClickedMenu } = this.state;
+    if (clickedMenu === "Intro") {
+      subMenu = [
+        { name: "회사소개", key: "/intro/ceo" },
+        { name: "경영 방침", key: "/intro/policy" },
+        { name: "회사 연혁", key: "/intro/history" },
+        { name: "오시는길", key: "/intro/come" },
+      ];
+    } else if (clickedMenu === "Info") {
+      subMenu = [
+        { name: "지입정보", key: "/Info/analysis" },
+        { name: "차량운용분석", key: "/Info/operation" },
+        { name: "자격증 가이드", key: "/Info/testguide" },
+      ];
+    } else if (clickedMenu === "Vehicle") {
+      subMenu = [
+        { name: "당사차량[1톤]", key: "/Vehicle/onevehicle" },
+        { name: "당사차량[2.5톤]", key: "/Vehicle/twovehicle" },
+        { name: "지입정보[4.5톤]", key: "/Vehicle/threevehicle" },
+        { name: "지입정보[5톤]", key: "/Vehicle/fourvehicle" },
+      ];
+    } else if (clickedMenu === "HanbitBoard") {
+      subMenu = [{ name: "공지사항", key: "/HanbitBoard" }];
     }
-    handleClickMenu = (clickedMenu) => {
-        const { clickedMenu: clickedMeneName, isClickedMenu } = this.state
-        if (clickedMeneName === clickedMenu && isClickedMenu) {
-            this.setState({ isClickedMenu: false })
-        } else {
-            this.setState({ clickedMenu, isClickedMenu: true })
-        }
+    return (
+      isClickedMenu && (
+        <div className="NaviContainer_submenu_container">
+          <div
+            className="NaviContainer_submenu_wrap"
+            onMouseLeave={this.leaveMenu}
+          >
+            {subMenu &&
+              subMenu.map((item) => {
+                return (
+                  <NavLink
+                    className="Submenu_link"
+                    key={item.key}
+                    to={item.key}
+                  >
+                    {item.name}
+                  </NavLink>
+                );
+              })}
+          </div>
+        </div>
+      )
+    );
+  };
+  render() {
+    const { clickedMenu } = this.state;
+    return (
+      <div className="NaviContainer">
+        {this.renderSubMenu(clickedMenu)}
 
-    }
-    renderSubMenu = (clickedMenu) => {
-        let subMenu;
-        const { isClickedMenu } = this.state
-        if (clickedMenu === "Intro") {
-            subMenu = [
-                { name: "회사소개", key: "/intro/ceo" },
-                { name: "경영 방침", key: "/intro/policy" },
-                { name: "회사 연혁", key: "/intro/history" },
-                { name: "오시는길", key: "/intro/come" }
-            ]
-        } else if (clickedMenu === "Info") {
-            subMenu = [
-                { name: "지입정보", key: "/Info/analysis" },
-                { name: "차량운용분석", key: "/Info/operation" },
-                { name: "자격증 가이드", key: "/Info/testguide" },
-            ]
-        }
-        else if (clickedMenu === "Vehicle") {
-            subMenu = [
-                { name: "당사차량[1톤]", key: "/Vehicle/onevehicle" },
-                { name: "당사차량[2.5톤]", key: "/Vehicle/twovehicle" },
-                { name: "지입정보[4.5톤]", key: "/Vehicle/threevehicle" },
-                { name: "지입정보[5톤]", key: "/Vehicle/fourvehicle" },
-            ]
-        }
-        else if (clickedMenu === "HanbitBoard") {
-            subMenu = [
-                { name: "공지사항", key: "/HanbitBoard" }
-            ]
-        }
-        return (
-            isClickedMenu && <div className="NaviContainer_submenu_container">
-                <div className="NaviContainer_submenu_wrap">
-                    {
-                        subMenu && subMenu.map(item => {
-                            return (
-                                <NavLink className="Submenu_link" key={item.key} to={item.key}>{item.name}</NavLink>
-                            )
-                        })
-                    }
-                </div>
+        <div className="NaviWrap">
+          <div className="TopNavi">
+            <div className="TopNaviWrap">
+              <div onClick={() => this.movePage("/")} className="Menu_Logo">
+                <img src={logo} alt="Logo"></img>
+              </div>
+              <Guide onClick={this.handleClickMenu} />
             </div>
-        )
-    }
-    render() {
-        const { clickedMenu } = this.state
-        return (
-            <div className="NaviContainer">
-                {
-                    this.renderSubMenu(clickedMenu)
+            <div className="Member_Box">
+              <BurMenu
+                className="Menu_Button"
+                right
+                customCrossIcon={<img src={closeicon} alt="closeicon" />}
+                customBurgerIcon={
+                  <img className="MenuIcon" src={menuicon} alt=""></img>
                 }
-
-                <div className="NaviWrap">
-                    <div className="TopNavi">
-                        <div className="TopNaviWrap">
-                            <div onClick={() => this.movePage("/")} className="Menu_Logo">
-                                <img src={logo} alt='Logo'></img>
-                            </div>
-                            <Guide onClick={this.handleClickMenu} />
-                        </div>
-                        <div className="Member_Box">
-                            <BurMenu className="Menu_Button" right customCrossIcon={<img src={closeicon} alt='closeicon' />} customBurgerIcon={<img className='MenuIcon' src={menuicon} alt=""></img>}>
-                                <NavLink exact={true} id="home" className="menu-item" to="/">한빛로지스</NavLink>
-                                <NavLink id="Company" className="menu-item" to="/Intro">회사소개</NavLink>
-                                <NavLink id="Info" className="menu-item" to="/Info">지입정보</NavLink>
-                                <NavLink id="Car" className="menu-item" to="/Vehicle">당사차량</NavLink>
-                                <NavLink id="Board" className="menu-item" to="/HanbitBoard">공지사항</NavLink>
-                            </BurMenu>
-                        </div>
-                    </div>
-                    
-                </div>
-
+              >
+                <NavLink exact={true} id="home" className="menu-item" to="/">
+                  한빛로지스
+                </NavLink>
+                <NavLink id="Company" className="menu-item" to="/Intro">
+                  회사소개
+                </NavLink>
+                <NavLink id="Info" className="menu-item" to="/Info">
+                  지입정보
+                </NavLink>
+                <NavLink id="Car" className="menu-item" to="/Vehicle">
+                  당사차량
+                </NavLink>
+                <NavLink id="Board" className="menu-item" to="/HanbitBoard">
+                  공지사항
+                </NavLink>
+              </BurMenu>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Navigation;
-
-
-
