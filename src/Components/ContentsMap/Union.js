65dc772858ff3d38202c54b945ec909390a164d9
jongho143 <jongho.lee@playlooc.com>
2020-08-13 14:12:00 +0900

    메인 명함신청 추가

--- src/Components/ContentsMap/Union.js
+++ src/Components/ContentsMap/Union.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
-import './style/Union.scss'
+import React, { Component } from "react";
+import "./style/Union.scss";
 
 class Union extends Component {
-    render() {
-        return (
-            <div className="UnionContainer">
-                <a href="tel:15888020">
-                    <div className="TextBox">
-                        <div className="Title_union">
-                            화물연합<br />
-                        운송
-                    </div>
-                        <div className="Text">
-                            운송차량이 필요할 땐
-                    </div>
-                    </div>
-                </a>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="UnionContainer">
+        <div className="TextBox">
+          <div className="Title_union">
+            명함제작
+            <br />
+            신청
+          </div>
+          <div className="Text">명함제작이 필요할 땐</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Union;
--- src/Components/MenuBox/InfoBox.js
+++ src/Components/MenuBox/InfoBox.js
@@ -1,48 +1,56 @@
-import React, { Component } from 'react';
-import './IntroductionBox.scss'
-import { NavLink } from 'react-router-dom';
-import './InfoBox.scss'
-import InfoDropDown from '../../Common/DropDown/InfoDropDown';
-import ArrowRight from '../../_asset/image/common/red-next-icon.png';
+import React, { Component } from "react";
+import "./IntroductionBox.scss";
+import { NavLink } from "react-router-dom";
+import "./InfoBox.scss";
+import InfoDropDown from "../../Common/DropDown/InfoDropDown";
+import ArrowRight from "../../_asset/image/common/red-next-icon.png";
 
 class InfoBox extends Component {
-    renderPc() {
-        return (
-            <div className="InfoBox">
-                <div className="Title">
-                    지입 정보
-                </div>
-                <NavLink className="InfoBox_navlink_container" to={`${this.props.match.path}/analysis`}>
-                    <div className="Line">지입 분석</div>
-                    {`${this.props.location.pathname}` === "/Info/analysis"  && <img src={ArrowRight} alt="arrowRight"></img>}
-                </NavLink>
-                <NavLink className="InfoBox_navlink_container" to={`${this.props.match.path}/operation`}>
-                    <div className="Line">차량운용분석</div>
-                    {`${this.props.location.pathname}` === "/Info/operation"  && <img src={ArrowRight} alt="arrowRight"></img>}
-                </NavLink>
-                <NavLink className="InfoBox_navlink_container" to={`${this.props.match.path}/testguide`}>
-                    <div className="Line">자격증 가이드</div>
-                    {`${this.props.location.pathname}` === "/Info/testguide"  && <img src={ArrowRight} alt="arrowRight"></img>}
-                </NavLink>
-            </div>
-        );
-    }
-    renderMobile = () => {
-        return (
-            <div className="IntroBox">
-                <InfoDropDown></InfoDropDown>
-            </div>
-
-        );
-    }
-    render() {
-        const { isMobile } = this.props
-        return (
-          <>
-            {isMobile ? this.renderMobile() : this.renderPc()}
-          </>
-        );
-      }
+  renderPc() {
+    return (
+      <div className="InfoBox">
+        <div className="Title">지입 정보</div>
+        <NavLink
+          className="InfoBox_navlink_container"
+          to={`${this.props.match.path}/analysis`}
+        >
+          <div className="Line">지입 분석</div>
+          {`${this.props.location.pathname}` === "/Info/analysis" && (
+            <img src={ArrowRight} alt="arrowRight"></img>
+          )}
+        </NavLink>
+        <NavLink
+          className="InfoBox_navlink_container"
+          to={`${this.props.match.path}/operation`}
+        >
+          <div className="Line">차량운용분석</div>
+          {`${this.props.location.pathname}` === "/Info/operation" && (
+            <img src={ArrowRight} alt="arrowRight"></img>
+          )}
+        </NavLink>
+        <NavLink
+          className="InfoBox_navlink_container"
+          to={`${this.props.match.path}/testguide`}
+        >
+          <div className="Line">자격증 가이드</div>
+          {`${this.props.location.pathname}` === "/Info/testguide" && (
+            <img src={ArrowRight} alt="arrowRight"></img>
+          )}
+        </NavLink>
+      </div>
+    );
+  }
+  renderMobile = () => {
+    return (
+      <div className="IntroBox">
+        <InfoDropDown></InfoDropDown>
+      </div>
+    );
+  };
+  render() {
+    const { isMobile } = this.props;
+    return <>{isMobile ? this.renderMobile() : this.renderPc()}</>;
+  }
 }
 
 export default InfoBox;
