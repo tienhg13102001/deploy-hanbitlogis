90924f9419b1fa977c744fbc0d51e30d6cfa174f
jongho143 <jongho.lee@playlooc.com>
2020-08-19 10:10:51 +0900

    명함 주문 링크 적용

--- src/Components/ContentsMap/Union.js
+++ src/Components/ContentsMap/Union.js
@@ -5,14 +5,16 @@ class Union extends Component {
   render() {
     return (
       <div className="UnionContainer">
-        <div className="TextBox">
-          <div className="Title_union">
-            명함제작
-            <br />
-            신청
+        <a href="https://www.notion.so/b8310ec859354d4f86fac0443d71d854">
+          <div className="TextBox">
+            <div className="Title_union">
+              명함제작
+              <br />
+              신청
+            </div>
+            <div className="Text">명함제작이 필요할 땐</div>
           </div>
-          <div className="Text">명함제작이 필요할 땐</div>
-        </div>
+        </a>
       </div>
     );
   }
